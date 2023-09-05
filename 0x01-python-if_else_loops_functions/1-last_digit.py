9f234999f3d4e6235888e508f8293c6791b4b778
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-09-05 12:43:41 -0700

    Updated the script that displays the last digit of random numbers.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 n = abs(number) % 10
 if n > 5:
-    print(f"Last digit of {number:d} is {n:d} iand is greater than 5")
+    print(f"Last digit of {number:d} is {n:d} and is greater than 5")
 elif n == 0:
     print(f"Last digit of {number:d} is {n:d} and is 0")
 else:
