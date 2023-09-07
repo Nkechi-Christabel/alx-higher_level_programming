ab50caf2a5ab4081bb208e0a3bc83776436e0e90
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-09-07 04:21:59 -0700

    Added a Readme file

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+
+if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+a = int(argv[1])
+op = argv[2]
+b = int(argv[3])
+
+ops = {'+': add,'-': sub,'*': mul,'/': div}
+
+if op in ops:
+    result = ops[op](a, b)
+    print("{:d} {:s} {:d} = {:d}".format(a, op, b, result))
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
