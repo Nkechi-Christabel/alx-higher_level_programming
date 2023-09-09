dd55886a4844b15526949fd4af99a27a83178f80
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-09-09 11:37:09 -0700

    Wrote a function that prints a matrix of integers.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,4 +7,4 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d} ".format(n), end='')
             else:
                 print("{:d}".format(n), end='')
-        print() 
+        print()
