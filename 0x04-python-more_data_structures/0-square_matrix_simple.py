d9d077b40ac6dc4dca81f9fa901c3074cdfe635c
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-09-13 04:28:45 -0700

    Wrote a function that returns a set of all elements present in only one set.

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
+""" A function that computes the square value of all integers of a matrix."""
+
+
 def square_matrix_simple(matrix=[]):
     return ([list(map(lambda n: n * n, row)) for row in matrix])
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
+"""A function that replaces all occurrences of an element by
+another in a new list."""
+
+
 def search_replace(my_list, search, replace):
     return (list(map(lambda str: replace if str == search else str, my_list)))
