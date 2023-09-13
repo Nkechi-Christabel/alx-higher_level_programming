b54a56190eb3ea3b6a0284cde118d22d072044c9
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-09-13 05:06:45 -0700

    Wrote  a function that replaces or adds key/value in a dictionary.

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""function that prints a dictionary by ordered keys."""
+
+
 def print_sorted_dictionary(a_dictionary):
     for k, v in sorted(a_dictionary.items()):
         print("{}: {}".format(k, v))
