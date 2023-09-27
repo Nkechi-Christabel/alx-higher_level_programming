63c84f77e93961b1f4a6c4380aa3eb97c5609b37
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-09-26 17:03:38 -0700

    Wrote a class Square that defines a square by: (based on 4-square.py)

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -13,17 +13,8 @@ class Square:
 
         Args:
             size (int): The size of the square. Defaults to 0 if not provided.
-
-        Raises:
-            TypeError: If the size is not a number (float or integer).
-            ValueError: If the size is less than 0.
         """
-        if not isinstance(size, (int, float)):
-            raise TypeError("size must be a number")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
         """
