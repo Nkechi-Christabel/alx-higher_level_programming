53efcccd8b05a2d7b811660f625c69d1e03fc97b
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-09-30 07:40:22 -0700

    Wrote a function that adds 2 integers.

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -21,9 +21,9 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If a or b is not an integer or float.
     """
-    if not isinstance(a, (int, float)):
+     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
+        raise TypeError("b must be an integer")i
 
     return (int(a) + int(b))
