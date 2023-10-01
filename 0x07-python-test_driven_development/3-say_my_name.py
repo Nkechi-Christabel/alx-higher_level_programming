fd3e8624b089968f88dfda9293e231c7fe77fb89
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-10-01 16:50:44 -0700

    Updated the function that prints My name is <first name> <last name>

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -9,6 +9,16 @@ My name is <first name> <last name>
 
 
 def say_my_name(first_name, last_name=""):
+    """
+    Prints the full name as "My name is <first name> <last name>".
+
+    Args:
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Default is an empty string.
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
