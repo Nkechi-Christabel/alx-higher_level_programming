9808422e6a6d00083579b3fb2c122620259a877d
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-10-02 05:45:17 -0700

    Wrote a function that prints a square with the character #.

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -35,7 +35,7 @@ def matrix_divided(matrix, div):
     if not all(isinstance(row, list) and all(isinstance(x, (int, float))
                                              for x in row) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists)"
-                        "of integers/floats")
+                        " of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+This is "4-print_square" module.
+
+The 4-print_square supplies one function, print_square(), that
+prints a square with the character #.
+"""
+
+
+def print_square(size):
+    """
+    Prints a square of '#' characters with the given size.
+
+    Args:
+        size (int): The size (length) of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    print('\n'.join(['#' * size for _ in range(size)]))
