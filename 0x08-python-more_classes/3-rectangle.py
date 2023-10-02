6e6ecf21a5a14acba80d91a294d2535579fbf941
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-10-02 12:27:16 -0700

    Wrote a class Rectangle that defines a rectangle by: (based on 2-rectangle.py)

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -8,8 +8,13 @@ It is based on the 2-rectangle.py module.
 
 
 class Rectangle:
-    """A class that defines a rectangle."""
+    """
+    This class represents a rectangle.
 
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+    """
     def __init__(self, width=0, height=0):
         """
         Initialize a new Rectangle instance.
