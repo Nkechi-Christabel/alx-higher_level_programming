faeb16221fa240add14acbae0fa41768f5adbd9c
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-10-07 01:44:43 -0700

    Updated print_square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -23,5 +23,6 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-
+    if size == 0:
+        return
     print('\n'.join(['#' * size for _ in range(size)]))
