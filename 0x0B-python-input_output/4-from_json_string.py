59a8586e9baff4e4467326a3d11ca31760975302
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-10-10 06:46:52 -0700

    Wrote a function that returns an object (Python data structure) represented by a JSON string:

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module (4-from_json_string.py) contains a function from_json_string(),
+that returns a Python data structure (object) represented by a JSON string.
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Deserialize a JSON-formatted string into a Python object.
+
+    Args:
+        my_str (str): The JSON-formatted string to be deserialized.
+
+    Returns:
+        Any: A Python data structure (object) represented by the JSON string.
+    """
+    return json.load(my_obj)
