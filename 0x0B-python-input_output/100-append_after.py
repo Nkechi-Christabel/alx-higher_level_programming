8cc01af43607f91277c34d8f772d2db5ac238b4a
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-10-10 09:42:49 -0700

    Wrote a function that inserts a line of text to a file, after each line containing a specific string

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+This module(100-append_after.py) contains function def pascal_
+triangle(n): that returns a list of lists of integers representing
+the Pascal’s triangle of n.
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Insert a line of text after each line containing a specific string in a file.
+
+    Args:
+        filename (str): The name of the text file.
+        search_string (str): The string to search for in each line.
+        new_string (str): The line of text to insert after lines containing the search string.
+
+    Example:
+        >>> append_after("example.txt", "specific", "Inserted line")
+    """
+    with open(filename, "r+") as file:
+        lines = file.readlines()
+        file.seek(0)
+
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string + "\n")
+        
+        file.truncate()
