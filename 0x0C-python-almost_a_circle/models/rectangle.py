b06c1f707c428186c6e274956438d8b9cda3c85f
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-10-12 14:31:57 -0700

    Updated  the class Rectangle by improving the public method def display(self): to print in stdout the Rectangle instance with the character # by taking care of x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,10 +16,21 @@ class Rectangle(Base):
         __height (int): The private height of the rectangle.
         __x (int): The private x-coordinate of the rectangle's position.
         __y (int): The private y-coordinate of the rectangle's position.
+
+      Methods:
+        __init__(self, width, height, x=0, y=0, id=None):
+            Initialize a new instance of the Rectangle class with validation.
+
+        area(self):
+            Calculate and return the area of the rectangle.
+
+        display(self):
+            Print the Rectangle instance using '#' characters to stdout, taking
+            x and y into account.
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-        Initialize a new instance of the Rectangle class.
+        Initialize a new instance of the Rectangle class with validation.
 
         Args:
             width (int): The width of the rectangle.
@@ -28,16 +39,16 @@ class Rectangle(Base):
             y (int, optional): The y-coordinate of the rectangle's position.
             id (int, optional): An identifier for the instance.
 
-        Attributes:
-            __width (int): The private width of the rectangle.
-            __height (int): The private height of the rectangle.
-            __x (int): The private x-coordinate of the rectangle's position.
-            __y (int): The private y-coordinate of the rectangle's position.
+        Raises:
+            TypeError: If width, height, x, or y is not an integer.
+            ValueError: If width or height is less than or equal to 0, or if x
+            or y is less than 0.
         """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
@@ -53,16 +64,19 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """
-        Setter for the width attribute.
+        Setter for the width attribute with validation.
 
         Args:
             value (int): The new width to set.
 
         Raises:
-            ValueError: If the provided width is less than or equal to 0.
+            TypeError: If the provided value is not an integer.
+            ValueError: If the provided value is less than or equal to 0.
         """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("Width must be greater than 0")
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -78,16 +92,19 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """
-        Setter for the height attribute.
+        Setter for the height attribute with validation.
 
         Args:
             value (int): The new height to set.
 
         Raises:
-            ValueError: If the provided height is less than or equal to 0.
+            TypeError: If the provided value is not an integer.
+            ValueError: If the provided value is less than or equal to 0.
         """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("Height must be greater than 0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -103,11 +120,19 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """
-        Setter for the x attribute.
+        Setter for the x attribute with validation.
 
         Args:
             value (int): The new x-coordinate to set.
+
+        Raises:
+            TypeError: If the provided value is not an integer.
+            ValueError: If the provided value is less than 0.
         """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -123,9 +148,46 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """
-        Setter for the y attribute.
+        Setter for the y attribute with validation.
 
         Args:
             value (int): The new y-coordinate to set.
+
+        Raises:
+            TypeError: If the provided value is not an integer.
+            ValueError: If the provided value is less than 0.
         """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """
+        Calculate and return the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle (width * height).
+        """
+        return self.__width * self.__height
+
+    def display(self):
+        """
+        Print the Rectangle instance using '#' characters to stdout.
+        """
+        for _ in range(self.__y):
+            print()
+        for _ in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
+
+    def __str__(self):
+        """
+        Return a string representation of the Rectangle.
+
+        Returns:
+            str: A string in the format [Rectangle] (<id>) <x>/<y> - <width>/
+            <height>.
+        """
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - "
+                f"{self.__width}/{self.__height}")
