97b5975fbb06b4f812c3c24cf56793bfba8a65d7
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-10-16 11:15:33 -0700

    Updated Reactangle test case

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-""" """
-
+"""
+Test cases for the Rectangle class in models.rectangle module.
+"""
 import unittest
 from models.rectangle import Rectangle
 
 class TestRectangleMethods(unittest.TestCase):
-    def test_init(self):
-        # Test initialization with valid arguments
+    """
+    A class to test methods and behaviors of the Rectangle class.
+    """
+        def test_init(self):
+        """
+        Test the initialization of a Rectangle.
+
+        Case 1: Test initialization with valid arguments.
+        """
         rect = Rectangle(4, 5, 1, 2, 10)
         self.assertEqual(rect.width, 4)
         self.assertEqual(rect.height, 5)
@@ -14,7 +22,9 @@ class TestRectangleMethods(unittest.TestCase):
         self.assertEqual(rect.y, 2)
         self.assertEqual(rect.id, 10)
 
-        # Test initialization with missing optional arguments
+        """
+        Case 2: Test initialization with missing optional arguments.
+        """
         rect2 = Rectangle(4, 5)
         self.assertEqual(rect2.width, 4)
         self.assertEqual(rect2.height, 5)
@@ -22,67 +32,104 @@ class TestRectangleMethods(unittest.TestCase):
         self.assertEqual(rect2.y, 0)  # Default value
 
     def test_width_property(self):
-        # Test valid width
+        """
+        Test the width property of a Rectangle.
+
+        Case 1: Test valid width.
+        """
         rect = Rectangle(4, 5)
         rect.width = 6
         self.assertEqual(rect.width, 6)
 
-        # Test invalid width (not an integer)
+        """
+        Case 2: Test invalid width (not an integer).
+        """
         with self.assertRaises(TypeError):
             rect.width = "invalid"
 
-        # Test invalid width (less than or equal to 0)
+        """
+        Case 3: Test invalid width (less than or equal to 0).
+        """
         with self.assertRaises(ValueError):
             rect.width = 0
 
     def test_height_property(self):
-        # Test valid height
+        """
+        Test the height property of a Rectangle.
+
+        Case 1: Test valid height.
+        """
         rect = Rectangle(4, 5)
         rect.height = 7
         self.assertEqual(rect.height, 7)
 
-        # Test invalid height (not an integer)
+        """
+        Case 2: Test invalid height (not an integer).
+        """
         with self.assertRaises(TypeError):
             rect.height = "invalid"
 
-        # Test invalid height (less than or equal to 0)
+        """
+        Case 3: Test invalid height (less than or equal to 0).
+        """
         with self.assertRaises(ValueError):
             rect.height = 0
 
     def test_x_property(self):
-        # Test valid x
+        """
+        Test the x property of a Rectangle.
+
+        Case 1: Test valid x.
+        """
         rect = Rectangle(4, 5)
         rect.x = 2
         self.assertEqual(rect.x, 2)
 
-        # Test invalid x (not an integer)
+        """
+        Case 2: Test invalid x (not an integer).
+        """
         with self.assertRaises(TypeError):
             rect.x = "invalid"
 
-        # Test invalid x (less than 0)
+        """
+        Case 3: Test invalid x (less than 0).
+        """
         with self.assertRaises(ValueError):
             rect.x = -1
 
     def test_y_property(self):
-        # Test valid y
+        """
+        Test the y property of a Rectangle.
+
+        Case 1: Test valid y.
+        """
         rect = Rectangle(4, 5)
         rect.y = 3
         self.assertEqual(rect.y, 3)
 
-        # Test invalid y (not an integer)
+        """
+        Case 2: Test invalid y (not an integer).
+        """
         with self.assertRaises(TypeError):
             rect.y = "invalid"
 
-        # Test invalid y (less than 0)
+        """
+        Case 3: Test invalid y (less than 0).
+        """
         with self.assertRaises(ValueError):
             rect.y = -1
 
     def test_area(self):
+        """
+        Test the area method of a Rectangle.
+        """
         rect = Rectangle(4, 5)
         self.assertEqual(rect.area(), 20)
 
     def test_display(self):
-        # Test display output
+        """
+        Test the display method of a Rectangle.
+        """
         rect = Rectangle(3, 2)
         expected_output = "###\n" \
                           "###\n"
@@ -91,11 +138,17 @@ class TestRectangleMethods(unittest.TestCase):
             self.assertEqual(mock_stdout.getvalue(), expected_output)
 
     def test_str(self):
+        """
+        Test the string representation of a Rectangle.
+        """
         rect = Rectangle(4, 5, 1, 2, 10)
         expected_output = "[Rectangle] (10) 1/2 - 4/5"
         self.assertEqual(str(rect), expected_output)
 
     def test_update(self):
+        """
+        Test the update method of a Rectangle.
+        """
         rect = Rectangle(4, 5, 1, 2, 10)
         rect.update(20, 6, 7, 3, 4)
         self.assertEqual(rect.id, 20)
@@ -105,10 +158,12 @@ class TestRectangleMethods(unittest.TestCase):
         self.assertEqual(rect.y, 4)
 
     def test_to_dictionary(self):
+        """
+        Test the to_dictionary method of a Rectangle.
+        """
         rect = Rectangle(4, 5, 1, 2, 10)
         expected_dict = {'id': 10, 'width': 4, 'height': 5, 'x': 1, 'y': 2}
         self.assertEqual(rect.to_dictionary(), expected_dict)
 
 if __name__ == '__main__':
     unittest.main()
-
