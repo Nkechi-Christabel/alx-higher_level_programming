5fa189bb35088374b3bb4d94a74dc73091e2303f
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-11-14 11:50:43 -0800

    Wrote a script that displays the max temperature of each state (ordered by State name).

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -3,5 +3,4 @@
 -- August ordered by temperature (descending)
 
 SELECT city, AVG(value) AS avg_temp FROM temperatures
-WHERE month IN (7, 8) GROUP BY city
-ORDER BY avg_temp DESC LIMIT 3;
+WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
