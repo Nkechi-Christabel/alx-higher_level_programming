456757148d9fd3822395c3b2623a1b652d5d0333
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-11-15 11:51:09 -0800

    Updated some scripts.

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -7,7 +7,7 @@
     -- You can use only one SELECT statement
     -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_genres.name FROM tv_genres
+SELECT name FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
