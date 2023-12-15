eb0635cc8db1f072fce7b6237abbe9a52318a9f2
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-12-15 10:57:47 -0800

    Wrote a script that adds the State object “Louisiana” to the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
 The script that adds the State object “Louisiana” to the database
-hbtn_0e_6_usa.
+hbtn_0e_6_usa and prints its ID using the with statement.
 """
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+
 
 if __name__ == "__main__":
     engine = create_engine(f'mysql://{argv[1]}:{argv[2]}@localhost:\
@@ -15,11 +16,11 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
-    session = Session()
 
-    lou_state = State(name="Louisiana")
-    session.add(lou_state)
-    session.commit()
+    with Session() as session:
+        new_state = State(name="Louisiana")
+        session.add(new_state)
 
-    print(lou_state.id)
-    session.close()
+        session.commit()
+
+        print(new_state.id)
