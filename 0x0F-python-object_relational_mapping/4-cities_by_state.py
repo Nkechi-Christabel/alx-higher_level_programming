f06d97735877d33555f6c11fba46159561df4239
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-12-15 02:56:54 -0800

    Updated cities by state and all cities by state scripts

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
                                  passwd=argv[2], db=argv[3])
     cursor = db_connect.cursor()
     cursor.execute("SELECT cities.id, cities.name, states.name FROM cities\
-                   INNER JOIN states ON cities.state_id = states.id ORDER BY id")
+                   INNER JOIN states ON cities.state_id = states.id\
+                   ORDER BY id")
     data = cursor.fetchall()
 
     for row in data:
