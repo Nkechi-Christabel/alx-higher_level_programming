9504842442fbfb756b087b1df06caf9579ceca57
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-12-15 17:09:24 -0800

    Wrote  a script that lists all City objects from the database hbtn_0e_101_usa

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+The script that lists all City objects from the database hbtn_0e_101_usa.
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine(f'mysql://{argv[1]}:{argv[2]}@localhost:\
+                           3306/{argv[3]}')
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    with Session() as session:
+        data = session.query(City).all()
+
+        for city in data:
+            print(f"{city.id}: {city.name} -> {city.state.name}")
