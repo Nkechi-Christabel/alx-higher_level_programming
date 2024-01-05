94ccd359d65015a3f3c412c7847124cf3644d3d7
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-05 13:14:46 -0800

    Wrote a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #It makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a You got me! message.
-curl -s -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
