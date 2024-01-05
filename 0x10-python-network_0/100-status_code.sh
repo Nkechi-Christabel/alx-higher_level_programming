ce843a24d84c43c986cebadadc248ffc52bf5a17
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-05 12:30:10 -0800

    Wrote a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # It sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -sI "$1" | head -n 1 | grep -oE '[0-9]{3}'
+curl -s -o /dev/null -w "%{http_code}" "$url"
