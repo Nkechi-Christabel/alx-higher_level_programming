cf3be1b028af8d1c3bab762f675533f08d48b4cf
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-06 15:01:14 -0800

    Wrote a script that lists  commits (from the most recent to oldest) of the repository “rails” by the user 'rails'

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This script lists 10 commits (from the most recent to oldest) of the
+repository “rails” by the user “rails”
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    r = requests.get(f'https://api.github.com/repos/{argv[2]}/{argv[1]}/commits')
+    if r.status_code == 200:
+        for commit in r.json()[:10]:
+            print(f"{commit['sha']}: {commit['commit']['author']['name']}")
