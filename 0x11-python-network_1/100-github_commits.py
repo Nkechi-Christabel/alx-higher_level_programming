aae786e4c7a837621e25399bd06569378f58fc95
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-06 15:08:31 -0800

    Wrote a script that lists  commits (from the most recent to oldest) of the repository “rails” by the user 'rails'

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,8 +7,8 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    r = requests.get(f'https://api.github.com/repos/{argv[2]}/{argv[1]}\
-                     /commits')
+    r = requests.get(f'https://api.github.com/repos/{argv[2]}/{argv[1]}'
+                     '/commits')
     if r.status_code == 200:
         for commit in r.json()[:10]:
             print(f"{commit['sha']}: {commit['commit']['author']['name']}")
